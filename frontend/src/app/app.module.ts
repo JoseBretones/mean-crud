7a7a40c43dfc3ecd1103ce745b3ba214f86139db
JoseBretones <joseantoniobretones.18@campuscamara.es>
2020-01-20 20:45:14 +0100

    configurating frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TeachersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
